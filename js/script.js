238199a647126e5c5f15e7f38fbd7301c67e93ff
Shahzad-Wazir <shahzadwazir0123@gmail.com>
2025-03-21 21:00:46 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 // typing Animation
 var typed = new Typed(".typing", {
-    strings: ["Web Designer", "Web Developer", "Full-stack Developer",""],
+    strings: ["Web Designer", "Web Developer", "Full-stack Developer", ""],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
- });
- 
+});
